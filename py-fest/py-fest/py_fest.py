095bd00c5535c414c741c41e39135921ac899e8d
dgjoj <dgjo296@learner.dvusd.org>
2026-01-12 13:56:32 -0700

    update on pyfest

--- py-fest/py-fest/py_fest.py
+++ py-fest/py-fest/py_fest.py
@@ -1,1 +1,21 @@
+# The initial lineup
+lineup = [
+    ("Code Play", "Indie", 30),
+    ("The Pythonistas", "Rock", 45),
+    ("Syntax Error", "Metal", 60)
+]
 
+# 1. Add the headliner
+headliner = ("The Byte Beats", "Synthwave", 90)
+lineup.append(headliner)
+
+# Continue the logic below...
+
+print(lineup)
+
+
+
+
+print("1. View Lineup & Total Time")
+print("2. Add a new band")
+print("3. Move first band to end(late arrival)")
